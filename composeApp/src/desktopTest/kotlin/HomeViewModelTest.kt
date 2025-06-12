2aa9286dc447566c02a44d1334cd76d4c8aaff65
Felipe <tournfelipe@gmail.com>
2025-06-12 12:38:28 -0300

    Test del HomeViewModel y dependencia para usar mockK

---
+++ composeApp/src/desktopTest/kotlin/HomeViewModelTest.kt
@@ -0,0 +1,72 @@
+import edu.dyds.movies.domain.entity.Movie
+import edu.dyds.movies.domain.entity.QualifiedMovie
+import edu.dyds.movies.domain.usecase.home.GetPopularMoviesUseCase
+import edu.dyds.movies.presentation.home.HomeViewModel
+import io.mockk.coEvery
+import io.mockk.mockk
+import junit.framework.TestCase.assertFalse
+import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.flow.take
+import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import kotlin.test.Test
+import kotlin.test.assertEquals
+
+class HomeViewModelTest {
+    private lateinit var useCase: GetPopularMoviesUseCase
+    private lateinit var viewModel: HomeViewModel
+
+    @Before
+    fun setUp() {
+        useCase = mockk()
+        viewModel = HomeViewModel(useCase)
+    }
+
+    @Test
+    fun `getAllMovies emits loading then movies`() = runTest {
+        //Arrange
+        val qMovie1 =
+            QualifiedMovie(
+                movie = Movie(
+                    id = 1,
+                    title = "Movie 1",
+                    overview = "Some overview",
+                    releaseDate = "2023-01-01",
+                    poster = "poster.jpg",
+                    backdrop = "backdrop.jpg",
+                    originalTitle = "Original Movie 1",
+                    originalLanguage = "en",
+                    popularity = 9.8,
+                    voteAverage = 8.7
+                ), true
+            )
+        val qMovie2 =
+            QualifiedMovie(
+                movie = Movie(
+                    id = 2,
+                    title = "Movie 2",
+                    overview = "Some overview 2",
+                    releaseDate = "2023-02-02",
+                    poster = "poster.jpg",
+                    backdrop = "backdrop.jpg",
+                    originalTitle = "Original Movie 2",
+                    originalLanguage = "es",
+                    popularity = 4.8,
+                    voteAverage = 5.7
+                ), false
+            )
+
+        val mockMovies = listOf(qMovie1, qMovie2)
+        coEvery { useCase.getAllMovies() } returns mockMovies
+
+        // Act
+        viewModel.getAllMovies()
+        val collectedStates = viewModel.moviesStateFlow.take(2).toList()
+
+        // Assert
+        assertTrue(collectedStates.first().isLoading)
+        assertFalse(collectedStates.last().isLoading)
+        assertEquals(mockMovies, collectedStates.last().movies)
+    }
+}
